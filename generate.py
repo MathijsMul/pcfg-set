237619267ce0d7b41a3806ca905c5cd8474aad95
Mathijs <mathijsmul@gmail.com>
2018-11-07 12:28:45 +0100

    transformed data

--- generate.py
+++ generate.py
@@ -1,50 +1,44 @@
 import argparse
 import random
+import tasks
+import sys
 
-# Unary functions
-def copy(string):
-    return(string)
-
-def reverse(string):
-    return(string[::-1])
-
-def shift(string):
-    return(string[1:] + [string[0]])
-
-def echo(string):
-    return(string + [string[-1]])
-
-# Binary functions
-def append(string1, string2):
-    return(string1 + string2)
-
-def prepend(string1, string2):
-    return(string2 + string1)
+sys.setrecursionlimit(50)
 
 class MarkovTree():
     """
     PCFG
     """
-    def __init__(self, alphabet, prob_unary, prob_func, lengths):
+    def __init__(self, unary_functions, binary_functions, alphabet, prob_unary, prob_func, lengths, placeholders, omit_brackets):
+        self.unary_functions = unary_functions
+        self.binary_functions = binary_functions
+        self.all_functions = self.unary_functions + self.binary_functions
+
         self.alphabet = alphabet
 
+        self.set_probabilities(prob_unary, prob_func)
+
+        self.lengths = lengths
+        self.string_arguments = []
+        self.arg_length_counts = {i : len(self.alphabet) ** i for i in self.lengths}
+
+        self.placeholders = placeholders
+        self.omit_brackets = omit_brackets
+
+    def set_probabilities(self, prob_unary, prob_func):
         self.prob_unary = prob_unary
         self.prob_binary = 1 - self.prob_unary
 
         self.prob_func = prob_func
         self.prob_str = 1 - self.prob_func
 
-        self.lengths = lengths
-
-        self.string_arguments = []
-
     def function_next(self):
         # Determine item following arbitrary function call
         if random.random() < self.prob_unary:
-            return([random.choice(UNARY), self.unary_next()])
+            return([random.choice(self.unary_functions), self.unary_next()])
         else:
             [arg1, arg2] = self.binary_next()
-            return([random.choice(BINARY), arg1, arg2])
+            return([random.choice(self.binary_functions), arg1, arg2])
 
     def unary_next(self):
         # Determine item following unary function call
@@ -65,13 +59,21 @@ class MarkovTree():
         return(next)
 
     def string_argument(self):
-        candidate = [random.choice(self.alphabet) for i in range(random.choice(self.lengths))]
-        if not candidate in self.string_arguments:
-            # make sure that the same string arguments do not occur
-            self.string_arguments += [candidate]
-            return(candidate)
+        if not self.placeholders:
+            candidate = [random.choice(self.alphabet) for i in range(random.choice(self.lengths))]
+            if not candidate in self.string_arguments:
+                # make sure that the same string arguments do not occur
+                self.string_arguments += [candidate]
+                return(candidate)
+            else:
+                return(self.string_argument())
         else:
-            return(self.string_argument())
+            candidate_len = random.choice(self.lengths)
+            if not self.arg_length_counts[candidate_len] == 0:
+                self.arg_length_counts[candidate_len] -= 1
+                return(['X' for i in range(candidate_len)])
+            else:
+                return(self.string_argument())
 
     def build(self):
         # Always start with function call
@@ -86,73 +88,99 @@ class MarkovTree():
         # Evaluate output
         if all(isinstance(item, str) for item in tree):
             return(tree)
-        if tree[0] in UNARY:
+        if tree[0] in self.unary_functions:
             return(tree[0](self.evaluate_tree(tree[1])))
-        elif tree[0] in BINARY:
+        elif tree[0] in self.binary_functions:
             return(tree[0](self.evaluate_tree(tree[1]), self.evaluate_tree(tree[2])))
 
     def write(self, tree):
         # Convert tree to string for data file
         if all(isinstance(item, str) for item in tree):
             return(' '.join(tree))
-        if tree[0] in UNARY:
-            return(tree[0].__name__ + ' ( ' + self.write(tree[1])) + ' )'
-        elif tree[0] in BINARY:
-            return(tree[0].__name__ + ' ( ' + self.write(tree[1]) + ' , ' + self.write(tree[2]) + ' )')
+        if tree[0] in self.unary_functions:
+            if self.omit_brackets:
+                return (tree[0].__name__ + ' ' + self.write(tree[1]))
+            else:
+                return(tree[0].__name__ + ' ( ' + self.write(tree[1])) + ' )'
+        elif tree[0] in self.binary_functions:
+            if self.omit_brackets:
+                return (tree[0].__name__ + ' ' + self.write(tree[1]) + ' , ' + self.write(tree[2]))
+            else:
+                return(tree[0].__name__ + ' ( ' + self.write(tree[1]) + ' , ' + self.write(tree[2]) + ' )')
 
-def generate_data(alphabet, prob_unary, prob_func, lengths, total_samples, train_ratio, data_root):
-    t = MarkovTree(alphabet=alphabet, prob_unary=prob_unary, prob_func=prob_func, lengths=lengths)
+def generate_data(task, random_probs, prob_unary, prob_func, lengths, total_samples, no_split, train_ratio, data_root, placeholders=False, omit_brackets=False):
+    # so far: same weight to functions within classes
+    unary_functions = task.unary_functions
+    binary_functions = task.binary_functions
+    alphabet = task.alphabet
 
-    train_file = data_root + '_train.txt'
-    test_file = data_root + '_test.txt'
+    t = MarkovTree(unary_functions=unary_functions,
+                   binary_functions=binary_functions,
+                   alphabet=alphabet,
+                   prob_unary=prob_unary,
+                   prob_func=prob_func,
+                   lengths=lengths,
+                   placeholders=placeholders,
+                   omit_brackets=omit_brackets)
 
-    with open(train_file, 'w') as f_train:
-        with open(test_file, 'w') as f_test:
-            for i in range(total_samples):
-                tree = t.build()
-                if random.random() < train_ratio:
-                    f_train.write(t.write(tree) + '\t' + ' '.join(t.evaluate_tree(tree)) + '\n')
+    if no_split:
+        file = open(data_root + '.txt', 'w')
+    else:
+        train_file = open(data_root + '_train.txt', 'w')
+        test_file = open(data_root + '_test.txt', 'w')
+
+    for i in range(total_samples):
+        if random_probs:
+            t.set_probabilities(prob_unary=random.random(),
+                                prob_func = random.random())
+                                #prob_func=random.uniform(0.0, 0.9)) # keep prob_func a bit low to prevent recursion errors
+        try:
+            tree = t.build()
+            written_tree = t.write(tree)
+            # Control maximum tree size
+            if len(written_tree) < 300:
+                if no_split:
+                    file.write(written_tree+ '\t' + ' '.join(t.evaluate_tree(tree)) + '\n')
                 else:
-                    f_test.write(t.write(tree) + '\t' + ' '.join(t.evaluate_tree(tree)) + '\n')
+                    if random.random() < train_ratio:
+                        train_file.write(written_tree + '\t' + ' '.join(t.evaluate_tree(tree)) + '\n')
+                    else:
+                        test_file.write(written_tree + '\t' + ' '.join(t.evaluate_tree(tree)) + '\n')
+        except RecursionError:
+            pass
 
 if __name__ == '__main__':
-    ALPHABET = ['A', 'B', 'C', 'D',
-                'E', 'F', 'G', 'H',
-                'I', 'J', 'K', 'L',
-                'M', 'N', 'O', 'P',
-                'Q', 'R', 'S', 'T',
-                'U', 'V', 'W', 'X',
-                'Y', 'Z']
-
-    PROB_UNARY = 0.75
-    #PROB_BINARY = 1 - PROB_UNARY
-    PROB_FUNC = 0.25
-    #PROB_STR = 1 - PROB_FUNC
-
-    LENGTHS = [2, 3, 4]  # TODO: make this adaptive, also dependent on distribution
-
-    # so far: same weight to functions within classes
-    UNARY = [copy, reverse, shift, echo]
-    BINARY = [append, prepend]
-    FUNC = UNARY + BINARY
-
     parser = argparse.ArgumentParser()
-    parser.add_argument('--alphabet', type=list, help='Alphabet', default=ALPHABET)
-    parser.add_argument('--prob_unary', type=float, help='P(unary|function)', default=PROB_UNARY)
-    parser.add_argument('--prob_func', type=float, help='P(function|argument)', default=PROB_FUNC)
-    parser.add_argument('--lengths', type=int, help='Lengths of string arguments', default=LENGTHS)
+    parser.add_argument('--task', type=str, help='The PCFG SET task to use', default='default')
+    parser.add_argument('--random_probs', action='store_true', help='Use different random probabilities for each sample')
+    # TODO: distribution input lengths
+    parser.add_argument('--prob_unary', type=float, help='P(unary|function)', default=0.75)
+    parser.add_argument('--prob_func', type=float, help='P(function|argument)', default=0.25)
+    parser.add_argument('--lengths', type=int, help='Lengths of string arguments', default=[2, 3, 4, 5]) # TODO: make this adaptive, also dependent on distribution
     parser.add_argument('--nr_samples', type=int, help='Number of samples to generate', default=2500)
+    parser.add_argument('--no_split', action='store_true', help='Do not split into train and test yet')
     parser.add_argument('--train_ratio', type=float, help='Fraction of generated data to use for training', default=0.8)
     parser.add_argument('--data_root', type=str, help='Data path root')
+    parser.add_argument('--placeholder_args', action='store_true', help='Generate data with placeholder arguments, containing only X characters')
+    parser.add_argument('--omit_brackets', action='store_true', help='Do not use brackets')
     opt = parser.parse_args()
 
     if not opt.data_root:
         parser.error('Data path root required.')
 
-    generate_data(alphabet=opt.alphabet,
+    if not opt.task:
+        parser.error('Specify PCFG SET task.')
+
+    task = getattr(tasks, opt.task)
+
+    generate_data(task=task,
+                  random_probs=opt.random_probs,
                   prob_unary=opt.prob_unary,
                   prob_func=opt.prob_func,
                   lengths=opt.lengths,
                   total_samples=opt.nr_samples,
+                  no_split=opt.no_split,
                   train_ratio=opt.train_ratio,
-                  data_root=opt.data_root)
+                  data_root=opt.data_root,
+                  placeholders=opt.placeholder_args,
+                  omit_brackets=opt.omit_brackets)
